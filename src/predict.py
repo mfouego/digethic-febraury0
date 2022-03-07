392e359e09e371802daa148382ee2389d48581e5
mfouego <marcissefouego@yahoo.fr>
2022-03-07 23:51:49 +0100

    DIG-296 add predict.py

---
+++ src/predict.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import pickle
+
+file_to_open = open("data/models/baumethoden_lr.pickle", "rb")
+trained_model = pickle.load(file_to_open)
+file_to_open.close()
+
+prediction_data = pd.read_csv('data/prediction-training-data.csv', sep=";")
+print(prediction_data)
+print(trained_model.predict(prediction_data))

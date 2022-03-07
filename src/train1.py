c51df956eec66b09fa878dd92859f03b99cef90f
mfouego <marcissefouego@yahoo.fr>
2022-03-07 21:29:30 +0100

    DIG-312 add data/models directory

---
+++ src/train1.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+data = pd.read_csv('data/auto-mpg-training-data.csv', 'sep')
+print(data)
+
+data = data.sample(frac=1)
+
+y_variable = data['mpg']
+x_variable = data.loc[:, data.columns != 'mpg']
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x_variable, y_variable, test_size=0.2)
+
+regressor = LinearRegression()
+
+regressor = regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+
+file_to_write = open("data/models/baumethoden_lr.pickle", "wb")
+pickle.dump(regressor, file_to_write)

7952d45da5740fc389ef5c7788d7221882fba4cb
mfouego <marcissefouego@yahoo.fr>
2022-04-01 18:52:26 +0200

    DIG-390  add predict_resource

--- src/api.py
+++ src/api.py
@@ -1,20 +1,24 @@
-from flask import Flask
-
+import pickle
+from flask import Flask, request
 import pandas as pd  # noqa
 import os  # noqa
 from flask_cors import CORS
-
+import numpy as np  # noqa
 from werkzeug import Response
+from io import StringIO
+
 
 app = Flask(__name__)
 
 CORS(app)
 
-# training_data = data = pd.read_csv('data\auto-mpg-training-data.csv', sep=";")
-# print(data)
+
 training_data = pd.read_csv(os.path.join('data', "auto-mpg-training-data.csv"))
 
-print(training_data)
+file_to_open = open(os.path.join(
+    'data', 'models', "baumethoden_lr.pickle"), 'rb')
+trained_model = pickle.load(file_to_open)
+file_to_open.close()
 
 
 @app.route("/")
@@ -25,3 +29,27 @@ def hello_world():
 @app.route("/training_data", methods=["GET"])
 def get_training_data():
     return Response(training_data.to_json(), mimetype="application/json")
+
+
+@app.route("/predict", methods=["GET"])
+def predict():
+    zylinder = request.args.get('zylinder')
+    ps = request.args.get('ps')
+    gewicht = request.args.get('gewicht')
+    beschleunigung = request.args.get('beschleunigung')
+    baujahr = request.args.get('baujahr')
+
+    csv_string = ",".join([zylinder, ps, gewicht, beschleunigung, baujahr])
+
+    csv_data = StringIO(csv_string)
+    attribute_names = [zylinder, ps, gewicht, beschleunigung, baujahr]
+    prediction_data = pd.read_csv(csv_data, names=attribute_names)
+    prediction = trained_model.predict(prediction_data)
+
+    print(prediction)
+    return{"result": prediction[0]}
+
+    # prediction = trained_model.predict(
+    #   [[zylinder, ps, gewicht, beschleunigung, baujahr]])
+    # print(prediction)
+    # return {"result": prediction[0]}
